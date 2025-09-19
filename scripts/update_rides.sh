784b637fa6cf6e77dfaa4fe2370698180513b9e0
Aaron Duong <duongaaron03@gmail.com>
2025-09-19 14:59:53 -0500

    update for this week's rides

--- scripts/update_rides.sh
+++ scripts/update_rides.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Run the Python script
-python3 ../api_allocate_modular/main.py
+python3 api_allocate_modular/main.py && \
 
 # Stage all changes
-git add .
+git add . && \
 
 # Commit with message
-git commit -m "update for this week's rides"
+git commit -m "update for this week's rides" && \
 
 # Push to remote
 git push
