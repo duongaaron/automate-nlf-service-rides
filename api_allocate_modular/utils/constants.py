375ffda76cabb3d5ed096111808a7367cc1b3487
Aaron Duong <duongaaron03@gmail.com>
2025-09-19 23:44:03 -0500

    update for this week's rides

--- api_allocate_modular/utils/constants.py
+++ api_allocate_modular/utils/constants.py
@@ -39,10 +39,12 @@ driver_required_riders_to = {
 }
 rider_groups_to = [
     # {"Camille wong", "seojin Kwon"}
+    
 ]
 
 driver_required_riders_back = {
     # "Seojin Kwon": {"Jane Yoo", "Josh Yang"},
+    "Camille": {"Claire Doh"},
 }
 rider_groups_back = [
     # {"Khang Le", "Aaron duong"}
